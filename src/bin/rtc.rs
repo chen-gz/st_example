e7976141b93dd8c856367765b2243099ddb449a1
Guangzong Chen <chen@ggeta.com>
2025-07-10 22:55:47 +0000

    update

--- src/bin/rtc.rs
+++ src/bin/rtc.rs
@@ -9,13 +9,7 @@ const ORANGE: gpio::GpioPort = gpio::PC7;
 #[cortex_m_rt::entry]
 fn main() -> ! {
     low_power::Executor::take().run(|spawner| {
-        clock::init_clock(
-            false,
-            false,
-            16_000_000,
-            true,
-            clock::ClockFreqs::KernelFreq160Mhz,
-        );
+        clock::init_clock(true, clock::ClockFreqs::KernelFreq160Mhz);
         BLUE.setup();
         ORANGE.setup();
         u5_lib::rtc::enable_rtc_read();
